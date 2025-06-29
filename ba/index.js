eb98aa632f8159883ebc679c4364a3ed632eba42
Nikhil Bansal <114744576+Nik26sal@users.noreply.github.com>
2025-06-29 05:38:18 +0530

    main

--- ba/index.js
+++ ba/index.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to My Server');
+});
+
 app.use("/user", userRoutes);
 
 app.use((err, req, res, next) => {
