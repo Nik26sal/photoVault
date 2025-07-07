95613f393cc3925813f60bc4459b188326e3ac2f
Nikhil Bansal <114744576+Nik26sal@users.noreply.github.com>
2025-07-07 07:44:56 +0530

    correct error

--- ba/middlewares/multer.js
+++ ba/middlewares/multer.js
@@ -1,7 +1,7 @@
 import multer from 'multer';
 import fs from 'fs';
 
-const tempDir = './public/temp';
+const tempDir = '../public/temp';
 
 if (!fs.existsSync(tempDir)) {
     fs.mkdirSync(tempDir, { recursive: true });
